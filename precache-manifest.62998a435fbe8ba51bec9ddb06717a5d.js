7d89bf825feea15e63ec1b9515206bd9ea97eb37
Gian Carlo Ferrari <fgian1969@gmail.com>
2019-12-11 17:21:36 +0100

    Updates

---
+++ precache-manifest.62998a435fbe8ba51bec9ddb06717a5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c67f412fb567ddc83fe81d8cf860f671",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "9d1521b4845a0c1a5ee8",
+    "url": "/monster-rolodex/static/css/main.573d843b.chunk.css"
+  },
+  {
+    "revision": "12ed7e1fae371b4cb936",
+    "url": "/monster-rolodex/static/js/2.e494a0c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.e494a0c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9d1521b4845a0c1a5ee8",
+    "url": "/monster-rolodex/static/js/main.62b7f37f.chunk.js"
+  },
+  {
+    "revision": "939c7cac93c69c649cbc",
+    "url": "/monster-rolodex/static/js/runtime-main.11039a90.js"
+  }
+]);
